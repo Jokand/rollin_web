b44c42f172c354f3308f12798e1fabbbc6e2da02
Jokand <84349256+Jokand@users.noreply.github.com>
2022-09-23 02:07:40 +0300

    правка

--- database/log.php
+++ database/log.php
@@ -3,14 +3,13 @@ include "db-connection.php";
 session_start();
 $login = $_POST['login'];
 $password = $_POST['password'];
-$users = $database->query('SELECT `email`, `password` FROM `users`')->fetchAll(PDO::FETCH_ASSOC);
+$users = $database->query('SELECT `id`, `email`, `password` FROM `users`')->fetchAll(PDO::FETCH_ASSOC);
 $_SESSION['entrance'] = false;
-var_dump($users);
 foreach ($users as $user) {
     // if (($login == $user['email'] || $login == $user['name']) && password_verify($password, $user['password'])) {
     if (($login == $user['email']) && password_verify($password, $user['password'])) {
-
         $_SESSION['entrance'] = true;
+        $_SESSION['user_id'] = $user['id'];
         header('Location: ../index.php');
         die;
     }
